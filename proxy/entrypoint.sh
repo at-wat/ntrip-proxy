506828da710d92909369131a8653f07546093e01
Atsushi Watanabe <atsushi.w@ieee.org>
2018-04-29 23:47:31 +0900

    Support non ssl NTRIP server

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -6,5 +6,6 @@ NTRIP_AUTH=`echo -n ${NTRIP_USER}:${NTRIP_PASS} | base64`
 
 sed -i "s/@NTRIP_AUTH@/${NTRIP_AUTH}/" /etc/nginx/conf.d/default.conf
 sed -i "s/@NTRIP_DOMAIN@/${NTRIP_DOMAIN}/" /etc/nginx/conf.d/default.conf
+sed -i "s/@NTRIP_HTTP_PROTOCOL@/${NTRIP_HTTP_PROTOCOL}/" /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
