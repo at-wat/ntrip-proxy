76fdad48a94bdd77d782c07cae9d9ced65cef30b
Atsushi Watanabe <atsushi.w@ieee.org>
2018-04-29 23:30:10 +0900

    Auth on proxy container

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -2,4 +2,9 @@
 
 set -eu
 
+NTRIP_AUTH=`echo -n ${NTRIP_USER}:${NTRIP_PASS} | base64`
+
+sed -i "s/@NTRIP_AUTH@/${NTRIP_AUTH}/" /etc/nginx/conf.d/default.conf
+sed -i "s/@NTRIP_DOMAIN@/${NTRIP_DOMAIN}/" /etc/nginx/conf.d/default.conf
+
 exec nginx -g "daemon off;"
